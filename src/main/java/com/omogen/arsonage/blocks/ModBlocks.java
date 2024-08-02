73bd2b2980404ed267d790a934e0a06bd669875b
Omogen <monsterpath132@gmail.com>
2024-08-03 02:23:55 +1000

    Create ModBlocks.java

---
+++ src/main/java/com/omogen/arsonage/blocks/ModBlocks.java
@@ -0,0 +1,18 @@
+package com.omogen.arsonage.blocks;
+
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+
+public class ModBlocks {
+
+	
+}
